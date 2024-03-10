5956de1682c32e174dadcf94e7478f369b6f26f4
HunterFenix <hunterfenixvii@protonmail.ch>
2024-03-10 17:29:21 +0100

    Added part of game logic (to finish) and constructor and special ability to the Ladro,Mago and Guerriero class.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Welcome to the Java Homework Project repository! This project was created for a 
 Overview
 
 TO-DO
+    finish the game class
+    implement a special ability for Ladro e Mago
+    implement inventory and usage of various item
+    
--- src/main/java/net/teapot/Mago.java
+++ src/main/java/net/teapot/Mago.java
@@ -1,4 +1,21 @@
 package net.teapot;
 
 public class Mago extends Personaggio{
+    private int standardMagicAttack;
+    Mago(int lifePoints, int physicalDefense, int physicalAttack, int magicDefense, int magicAttack) {
+        setLifePoints(lifePoints);
+        setPhysicalDefense(physicalDefense);
+        setPhysicalAttack(physicalAttack);
+        setMagicDefense(magicDefense);
+        setMagicAttack(magicAttack);
+    }
+
+    // Special ability
+
+    public void magicWand(int magicAttack) {
+
+    }
+    public void endMagicWand() {
+
+    }
 }
